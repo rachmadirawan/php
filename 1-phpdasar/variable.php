4fcaf7b4bde813c164cbba4bcff5280eaf5c9fa0
rachmad irawan <gol3.4.dtsproa@gmail.com>
2022-10-12 13:55:59 +0700

    menambahkan tutorial variable di php dasar

---
+++ 1-phpdasar/variable.php
@@ -0,0 +1,13 @@
+<?php 
+//contoh variable
+//variable di awali dengan $
+//variable dapat menyimpan value / nilai
+
+$a = ''; //ini adalah variable $a sengan nila string kosong
+
+$a = "Rachmad Irawan"; // disini $a menyimpan nila string "Rachmad Irawan" yang menimpa $a = '';
+
+echo $a;
+echo "<br>";
+var_dump($a);
+
